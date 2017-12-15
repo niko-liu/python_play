ee6acb28019b41a0cf372fe372ecacd1bf640a00
niko.liu <niko.liu@vipshop.com>
2017-12-15 17:36:00 +0800

    修改

--- nkother/parent.py
+++ nkother/parent.py
@@ -18,14 +18,14 @@ class Parent():
         self.name = name
     
     def show_name(self):
-        print self.name
+        print(self.name)
 
 class Child(Parent):
     def __init__(self, name, toys_num):
         Parent.__init__(self, name);
         self.toys_num = toys_num
     def show_name(self):
-        print self.name + "," + str(self.toys_num)
+        print(self.name + "," + str(self.toys_num))
         
 parent = Parent("cat")
 child = Child("dog", 2)
