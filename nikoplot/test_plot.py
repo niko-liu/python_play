0be42f180cb213d49096f58cf18a5548ed267842
niko.liu <niko.liu@vipshop.com>
2017-12-21 10:53:58 +0800

    修改

---
+++ nikoplot/test_plot.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
+
+np.random.seed(19680801)
+
+# example data
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+x = mu + sigma * np.random.randn(437)
+
+num_bins = 50
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, normed=1)
+
+# add a 'best fit' line
+y = mlab.normpdf(bins, mu, sigma)
+ax.plot(bins, y, '--')
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
